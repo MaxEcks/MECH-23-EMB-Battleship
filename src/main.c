adc2e73f27c35d456a444905638de4bfdc16651e
MaxEcks <em9298@mci4me.at>
2025-06-13 01:42:32 +0200

    worked on main.c, one game was successfully played --> Losing-Handling not implemented yet

--- src/main.c
+++ src/main.c
@@ -1,33 +1,22 @@
-/**
-* @brief bla bla bla
-*
-* @details
-* - bla bla bla
-* - bla bla bla
-*
-*/
-
-// =========================================================================
-// Libraries
-
 #include <stm32f0xx.h>
 #include "clock_.h"
 #include <stdio.h>
 #include <stdbool.h>
+#include <string.h>
+#include <stdlib.h>
 // =========================================================================
-// Defines
-
 // LOG() macro instead of printf()
 #define LOG( msg... ) printf( msg );
 
 // Baudrate for USART communication
 #define BAUDRATE 115200
+
 // battlefield size
 #define FIELD_SIZE 100
 #define ROWS 10
 #define COLS 10
+#define CELL (x * 10 + y)
 // =========================================================================
-
 // For supporting printf function we override the _write function to redirect the output to UART
 int _write(int handle, char* data, int size) {
   // 'handle' is typically ignored in this context, as we're redirecting all output to USART2
@@ -54,7 +43,6 @@ int _write(int handle, char* data, int size) {
   // Return the total number of bytes that were written
   return size;
 }
-
 // =========================================================================
 // FIFO buffer implementation
 
@@ -100,6 +88,27 @@ int fifo_get(Fifo_t* fifo, uint8_t* data) {
     return 0;                                   // Read successful Return 0
 }
 // =========================================================================
+// finite state machine
+
+void state_init(void);
+void state_play(void);
+void state_end(void);
+
+void initializeSM(void);
+
+typedef enum {STATE_INIT, STATE_PLAY, STATE_END} State_Type;
+static void (*state_table[])(void)={state_init, state_play, state_end};
+static State_Type curr_state; /* The "current state" */
+
+// enemy attack ENUM:
+typedef enum {
+    HIT,
+    MISS,
+    LOST,
+    ATTACK_ERROR
+} AttackedCellType;
+
+// =========================================================================
 // global variables
 
 volatile Fifo_t usart_rx_fifo;
@@ -119,7 +128,13 @@ uint8_t my_field[100] = {
     0,0,0,0,0,2,2,0,3,0,
     4,4,4,4,0,0,0,0,0,0
 };
-uint8_t checksum[ROWS] = {0};
+uint8_t enemy_field[100];
+uint8_t my_checksum[ROWS] = {0};
+uint8_t enemy_checksum[ROWS] = {0};
+
+uint8_t enemy_hits = 0;
+int last_shot_x = 0;
+int last_shot_y = 0;
 
 char enemy_shots[FIELD_SIZE];   // enemy fire
 char my_shots[FIELD_SIZE];      // our fire
@@ -127,12 +142,14 @@ char my_shots[FIELD_SIZE];      // our fire
 const uint8_t USART2_TX_PIN = 2; // PA2 is used as USART2_TX
 const uint8_t USART2_RX_PIN = 3; // PA3 is used as USART2_RX
 // =========================================================================
+// Functionprototypes
+// eventuell void Funktionen umbauen auf int function(void){} für besseres Fehlerhandling
+// eventuell Funktionen noch anpassen --> nicht direkt auf globale Variablen schreiben, sondern über Zeiger!
 
-/**
- * @brief bla bla bla
- * @param bla bla
- * @return bla bla bla
- */
+void fifo_parser(void);
+void calculate_checksum(void);
+AttackedCellType check_game_status(void);
+void attacking_opponent(void);
 // =========================================================================
 // main loop
 
@@ -172,23 +189,13 @@ int main(void) {
 
     fifo_init((Fifo_t *)&usart_rx_fifo);                       // Init the FIFO
 
+    initializeSM();
+
     while (1) 
     {
         fifo_parser();
-        if(new_msg_ready && strcmp(msg_buffer, "HD_START") == 0) {
-            LOG("DH_START_MAX\r\n");
-            // create my_field and checksum
-            calculate_checksum();
-            new_msg_ready = false;
-        }
-        if(new_msg_ready && strncmp(msg_buffer, "HD_CS_", 6) == 0) {
-            LOG("DH_CS_");
-            for (int i = 0; i < 10; i++) {
-                LOG("%d", checksum[i]);
-            }
-            LOG("\r\n");
-            new_msg_ready = false;
-        }
+
+        state_table[curr_state]();
     }
 
     return 0;
@@ -206,7 +213,125 @@ void USART2_IRQHandler(void)
   }
 }
 // =========================================================================
-// Parser function
+// functions
+
+void state_init (void){
+    // ----- Start-Messages -----
+    // Input:  HD_START
+    // Output: DH_START_MAX
+    if(new_msg_ready && strcmp(msg_buffer, "HD_START") == 0) {
+        LOG("DH_START_MAX\r\n");
+        //create_my_field();
+        calculate_checksum();
+        new_msg_ready = false;
+
+        curr_state = STATE_INIT; // kein State-Wechsel
+    }
+    // ----- Exchange Checksum -----
+    // Input:  HD_CS_{xxxxxxxxxx}
+    // Output: DH_CS_{xxxxxxxxxx}
+    else if(new_msg_ready && strncmp(msg_buffer, "HD_CS_", 6) == 0){
+        // safe enemy checksum
+        for (uint8_t i = 0; i < ROWS; i++) {
+            enemy_checksum[i] = msg_buffer[i + 6] - '0';
+        }
+        
+        LOG("DH_CS_");
+        for (int i = 0; i < 10; i++) {
+            LOG("%d", my_checksum[i]);
+        }
+        LOG("\r\n");
+        new_msg_ready = false;
+
+        curr_state = STATE_PLAY;
+    }
+}
+
+void state_play(void){
+    // ----- Enemy lost the Game ----
+    // Input:  HD_SF_{row}D{xxxxxxxxxx} (10 times)
+    // Output: DH_SF_{row}D{xxxxxxxxxx} (10 times)
+    if(new_msg_ready && strncmp(msg_buffer, "HD_SF", 5) == 0 && msg_buffer[5] - '0' < ROWS){
+        // safe enemy field
+        uint8_t row = msg_buffer[5] - '0';
+        
+        // safe_enemy_field();  -> in Funktion auslagern
+        for (uint8_t col = 0; col < COLS; col++) {
+            enemy_field[row * 10 + col] = msg_buffer[7 + col];
+        }
+
+        new_msg_ready = false;
+
+        if (row < 9) {
+            curr_state = STATE_PLAY;
+        } else {
+            // calculate_cs(enemy_field);   -> CS des gegnerischen Spielfelds bilden
+            // return_field(my_field);      -> Ausgabe meines Spielfelds per UART
+            curr_state = STATE_END;
+        }
+    }
+    // ----- Enemy attack -----
+    // INPUT:  HD_BOOM_{x}_{y}
+    // OUTPUT: DH_BOOM_H/M and DH_BOOM_{x}_{y} OR DH_SF_{row}D{xxxxxxxxxx} (10 times), if we lost the game 
+    else if(new_msg_ready && strncmp(msg_buffer, "HD_BOOM_", 8) == 0 && strlen(msg_buffer) == 11){
+        // check if we were HIT
+        AttackedCellType status = check_game_status();
+        
+        switch (status)
+        {
+        case MISS:
+            LOG("DH_BOOM_M\r\n");
+            attacking_opponent();
+            curr_state = STATE_PLAY;
+            break;
+        
+        case HIT:
+            LOG("DH_BOOM_H\r\n");
+            attacking_opponent();
+            curr_state = STATE_PLAY;
+            break;
+
+        case LOST:
+            // return_field(my_field);
+            curr_state = STATE_END;
+            break;
+        
+        case ATTACK_ERROR:
+            // Vielleicht späteres Error-Handling implementieren
+            break;
+        
+        default:
+            break;
+        }
+
+        new_msg_ready = false;
+    }
+    // ----- Enemy Hit or Miss -----
+    // IN:  HD_BOOM_H or HD_BOOM_M
+    if(new_msg_ready && strncmp(msg_buffer, "HD_BOOM_", 8) == 0 && strlen(msg_buffer) == 9) {
+        // H oder M in my_shots schreiben
+        if(msg_buffer[8] == 'H') {
+            my_shots[last_shot_x * 10 + last_shot_y] = 'H';
+        } else {
+            my_shots[last_shot_x * 10 + last_shot_y] = 'M';
+        }
+        new_msg_ready = false;
+
+        curr_state = STATE_PLAY;
+    }
+}
+
+void state_end(void){
+    return;
+    // curr_state = STATE_INIT;
+}
+
+
+void initializeSM(void)
+{
+    curr_state = STATE_INIT;
+    // reset, gamefield, etc. ...
+}
 
 void fifo_parser(void)
 {
@@ -244,6 +369,46 @@ void calculate_checksum(void)
                 cs++;
             }
         }
-        checksum[row] = cs;
+        my_checksum[row] = cs;
     }
+}
+
+AttackedCellType check_game_status(void){
+    int x = msg_buffer[8] - '0';    // row
+    int y = msg_buffer[10] - '0';   // column
+
+    if (msg_buffer[8] < '0' || msg_buffer[8] > '9' || msg_buffer[10] < '0' || msg_buffer[10] > '9') {
+        return ATTACK_ERROR;
+    }
+
+    if (my_field[CELL] == 0) {
+        if(enemy_shots[CELL] != 'M') {
+            enemy_shots[CELL] = 'M';
+        }
+
+        return MISS;
+    } else {
+        if(enemy_shots[CELL] != 'H') {
+            enemy_shots[CELL] = 'H';
+            enemy_hits++;
+        }
+        
+        if (enemy_hits == 30) {
+            return LOST;
+        } else {
+            return HIT;
+        }
+    }
+}
+
+void attacking_opponent(void) {
+    int x;
+    int y;
+    do {
+        x = rand() % 10;           // Zeile 0–9
+        y = rand() % 10;           // Spalte 0–9
+    } while (my_shots[CELL] == 'H' || my_shots[CELL] == 'M');
+    LOG("DH_BOOM_%d_%d\r\n", x, y);
+    last_shot_x = x;
+    last_shot_y = y;
 }
